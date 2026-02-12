45a8ddb9c2347f9bfb3ae24c251a594b6ab5e046
Jaydeep Sahre <sahrejaydeep05@gmail.com>
2026-02-12 08:46:43 +0530

    fix Admin module

--- src/main/java/com/grownited/controller/AdminController.java
+++ src/main/java/com/grownited/controller/AdminController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 	
-	@GetMapping(value = {"/admin-dashboard","/"})
+	@GetMapping(value = {"/","/admin-dashboard"})
 	public String adminDashboard() {
 		return "AdminDashboard";
 	}
